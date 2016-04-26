4e7ee5a1be25802061177e5b46a6fbce6b68b557
ying <whydang@uw.edu>
2016-04-26 13:01:16 -0700

    add simple test methods

--- Klotski.py
+++ Klotski.py
@@ -1,5 +1,27 @@
-# Ying Dang
+# Ying Dang and Derek Rhodehamel
 # CSE 415 A, Spring 2016
 # Homework 4: Part I
 
 #2. BFS COMPLETE
+
+# determines whether or not the state is a goal state
+def goal_state(state):
+    return state[13] == '_' and state[14] == '_' and \
+           state[17] == '_' and state[18] == '_'
+
+
+# determines whether or not the two states are indentical by value
+def DEEP_EQUALS(s1,s2):
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            return False
+    return True
+
+
+# Performs an appropriately deep copy of a state,
+# for use by operators in creating new states.
+def copy_state(s):
+    new_state = list(s)
+    return new_state
+
+
