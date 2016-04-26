38ac00a5d24ebf80ad947fc93c2b4dfde2ea4ac1
Derek Rhodehamel <drhodeham@hotmail.com>
2016-04-26 12:42:32 -0700

    Added initial state

---
+++ Start_States.py
@@ -0,0 +1,8 @@
+# Shapes:
+# A B B C
+# A B B C
+# D E E F
+# D G H F
+# I _ _ J
+
+CREATE_INITIAL_STATE = lambda : ["A", "B", "B", "C", "A", "B", "B", "C", "D", "E", "E", "F", "D", "G", "H", "F", "I", "_", "_", "J"]
