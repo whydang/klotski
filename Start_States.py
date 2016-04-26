2e1920fca6a35a9a5ee9d116ff4ea0eae96304ec
Derek Rhodehamel <drhodeham@hotmail.com>
2016-04-26 12:54:16 -0700

    toString in Start_States

--- Start_States.py
+++ Start_States.py
@@ -6,3 +6,15 @@
 # I _ _ J
 
 CREATE_INITIAL_STATE = lambda : ["A", "B", "B", "C", "A", "B", "B", "C", "D", "E", "E", "F", "D", "G", "H", "F", "I", "_", "_", "J"]
+
+def toString(state):
+    result = ""
+    for row in range(5):
+        result += "   "
+        for col in range (4):
+            result += (state[4*row + col] + " ")
+        result+="\n"
+    return result
+
+save = toString(CREATE_INITIAL_STATE())
+print(save)
